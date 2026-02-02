8ce5e69aee3344e8c8f98535e03b8fdb9f824912
Md Aminur Islam <147307658+aminurislamdc@users.noreply.github.com>
2026-02-02 16:32:25 +0600

    A Simple Calculator

---
+++ Python/A Simple Calculator.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+result = num1 + num2
+
+# Show the result
+print(f"The sum is: {result}")
